c4c41ee3529df9a716e7abfc2f762edf0fab3f20
Umut Yalçınkaya <yalcinkayaumut@outlook.com>
2021-11-24 22:13:03 +0100

    Extended inheritance

--- my_package/my_package.py
+++ my_package/my_package.py
@@ -1,22 +1,44 @@
 class Parent:
     def __init__(self):
+        """
+        Parent Class
+        :return: parent_attribute <str>
+        """
         self.parent_attribute = 'I am a parent'
 
     def parent_method(self):
-        print('Back in my day...')
+        """
+        Parent Class
+        :return: partner_attribute <str>
+        To call the `parent_method()` use following line:
+        >>> child.parent_method()
+        """
+        self.partner_attribute = 'Back in my day...'
 
 # Create a child class that inherits from Parent
 class Child(Parent):
     def __init__(self):
+        """
+        Child Class
+        :return: child_attribute <str>
+        """
         Parent.__init__(self)
         self.child_attribute = 'I am a child'
 
 class SuperChild(Parent):
     def __init__(self):
+        """
+        SuperChild Class
+        :return: superchild_attribute <str>
+        """
         super().__init__()
         self.superchild_attribute = 'I am a superchild'
 
 class GrandChild(SuperChild):
     def __init__(self):
+        """
+        GrandChild Class
+        :return: grandchild_attribute <str>
+        """
         super().__init__()
         self.grandchild_attribute = 'I am a grandchild'
